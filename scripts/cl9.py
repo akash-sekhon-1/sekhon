1e8a4029f925e8b28d4fc68b187351f2c66961e2
akash-sekhon-1 <akashdeep4900@gmail.com>
2025-12-17 21:34:37 +0530

    update scripts/cl9.py Wed Dec 17 21:34:35 2025

--- scripts/cl9.py
+++ scripts/cl9.py
@@ -87,6 +87,7 @@ LOCAL_T_DUP_CHECK_JSON  = poin(LOCAL_JSON_DIR, "task_dup_check.json")
 LOCAL_LOG_PATH          = poin(LOCAL_JSON_DIR, "log_err.txt")
 LOCAL_CAREER_NEWS       = poin(LOCAL_JSON_DIR, "career_news.json")
 LOCAL_CREDS_PATH         = poin(LOCAL_JSON_DIR, ".creds")
+LOCAL_BABEL_PATH        = poin(LOCAL_JSON_DIR, 'babel_draw.json.gz')
 
 
 LOCAL_TEXT_HISTORY =    poin(LOCAL_JSON_DIR, ".text_history.json.gz") # THIS path is also mentioned in ins_adder.py (not imported to keep that fast)
